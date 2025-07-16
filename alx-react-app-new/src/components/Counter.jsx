7729e5a5ff9cbc8ab53d65cbd3b2ba6a9c8b1dd7
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2025-07-16 13:48:06 +0100

    Added alx-react-app-new and alx-react-app-props projects

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -7,6 +7,8 @@ function Counter() {
     <div style={{ textAlign: 'center', marginTop: '30px' }}>
       <p style={{ fontSize: '20px' }}>Current Count: {count}</p>
       <button onClick={() => setCount(count + 1)} style={{ marginRight: '10px' }}>
+        Increment</button>
+      <button onClick={() => setCount(count - 1)} style={{ marginRight: '10px' }}>
         Decrement</button>
       <button onClick={() => setCount(0)}>Reset</button>
       </div>
