0b2bf36d7cec6ef83ea11e78b52ff2e05fc2aab1
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2025-07-16 12:41:49 +0100

    Added new styled project folder: alx-react-app-new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// src/Footer.jsx
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: '#fff', textAlign: 'center', 
+    padding: '10px' }}>
+  <p>© 2023 City Lovers</p>
+</footer>
+  );
+}
+
+export default Footer;
+// src/components/Footer.jsx
+// src/Footer.jsx
