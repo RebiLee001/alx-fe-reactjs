1c5d8e77e5019fa8c8f902d55fc3bf17a584cc9e
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2025-07-19 12:47:32 +0100

    Update alx-react-app-props with recent changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// src/Footer.jsx
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
+// src/components/Footer.jsx
+// src/Footer.jsx
