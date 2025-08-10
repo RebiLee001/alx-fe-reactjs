f5f9c32ae0a913b25dcb91ba0b1d0049559ea9e7
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2025-08-10 16:19:21 +0100

    Update tailwind-react-integration with latest local changes

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -3,11 +3,10 @@ import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-500">
-      Tailwind CSS is working! 🎉
-    </h1>
-  )
+    <div>
+      <UserProfile />
+    </div>
+  );
 }
 
 export default App;
-import React from 'react';
