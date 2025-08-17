67fd97168ca1a43726ee0eff68b52a67da1f039e
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2025-08-17 06:47:23 +0100

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,29 +1,42 @@
-import React, { useState } from 'react'
-import HomePage from './components/HomePage'
-import RecipeDetail from './components/RecipeDetail'
-import AddRecipeForm from './components/AddRecipeForm'
-import data from './data.json'
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
+import AddRecipeForm from "./components/AddRecipeForm";
+import data from "./data.json";
 
 function App() {
-  const [recipes, setRecipes] = useState(data)
-  const [selectedRecipe, setSelectedRecipe] = useState(null)
+  const [recipes, setRecipes] = useState(data);
 
   const handleAddRecipe = (recipe) => {
-    setRecipes([...recipes, recipe])
-  }
+    setRecipes([...recipes, recipe]);
+  };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      {selectedRecipe ? (
-        <RecipeDetail recipe={selectedRecipe} onBack={() => setSelectedRecipe(null)} />
-      ) : (
-        <>
-          <HomePage recipes={recipes} onSelect={setSelectedRecipe} />
-          <AddRecipeForm onAdd={handleAddRecipe} />
-        </>
-      )}
-    </div>
-  )
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100 p-4">
+        <Routes>
+          {/* Homepage */}
+          <Route
+            path="/"
+            element={<HomePage recipes={recipes} onSelect={() => {}} />}
+          />
+
+          {/* Recipe detail page */}
+          <Route
+            path="/recipe/:id"
+            element={<RecipeDetail recipes={recipes} />}
+          />
+
+          {/* Add new recipe */}
+          <Route
+            path="/add"
+            element={<AddRecipeForm onAdd={handleAddRecipe} />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
