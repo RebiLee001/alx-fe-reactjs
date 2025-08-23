e17bb954b889e51cd6a0e96f034dd465e096b8ea
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2025-08-23 18:25:57 +0100

    Add Formik integration with formikForm.js and fix controlled RegistrationForm

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css'
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/FormikForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
